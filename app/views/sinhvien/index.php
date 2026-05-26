cfbcbc1be4067acc8c5403b315c61cf43f016f4d
trinh quan <quantd0106@gmail.com>
2026-05-26 22:26:37 +0700

    show dssv

--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $title ?></title>
 </head>
+<style>
+    table, th, td {
+        text-align: center;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 10px;
+    }
+    th {
+        background-color: #456fc8;
+        color: white;
+    }
+
+</style>
 <body>
-    <h1> Danh sách sinh vien</h1>
+
+    <h1><?php echo $title ?></h1>
+    <table >
+        <tr >
+            <th>id</th>
+            <th>Ho va ten</th>
+            <th>Gioi tinh</th>
+            <th>mssv</th>
+        </tr>
+        <?php foreach ($sinhvien as $sv) { ?> 
+            <tr>
+                <td> <?php echo $sv['id']; ?> </td>
+                <td> <?php echo $sv['sinhvien']; ?> </td>
+                <td> <?php echo $sv['giotinh']; ?> </td>
+                <td> <?php echo $sv['mssv']; ?> </td>
+            </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
